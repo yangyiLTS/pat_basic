49aa883d38aa17f4a69fecf883b6518aa9bef62f
yy <641016261@qq.com>
2018-01-27 17:57:36 +0800

    finish 1048

--- 1048.cpp
+++ 1048.cpp
@@ -16,21 +16,43 @@ int main() {
 	alen = A.length();
 	blen = B.length();
 	diff = blen - alen;
-	char C[109];
-	C[blen] = '\0';
-	for (int i = blen - 1; i >= 0; i--) {
-		if ((i - diff) < 0) {
-			C[i] = B[i];
-			continue;
+	char C[110];
+
+	C[alen > blen? alen : blen] = '\0';
+	if (blen >= alen) {
+		for (int i = blen - 1; i >= 0; i--) {
+			if ((i - diff) < 0) {
+				C[i] = B[i];
+				continue;
+			}
+			int a, b;
+			b = int(B[i] - 48);
+			a = int(A[i - diff] - 48);
+			if ((blen + i) % 2 == 1) {
+				C[i] = L[(a + b) % 13];
+			}
+			else {
+				C[i] = char((b - a) < 0 ? (b - a + 10 + 48) : (b - a + 48));
+			}
 		}
-		int a, b;
-		b = int(B[i] - 48);
-		a = int(A[i - diff] - 48);
-		if ((blen + i) % 2 == 1) {
-			C[i] = L[(a + b) % 13];
-		}
-		else {
-			C[i] = char((b - a) < 0 ? (b - a + 10 + 48) : (b - a + 48));
+	}
+	else {
+		for (int i = alen - 1; i >= 0; i--) {
+			int a, b;
+			a = int(A[i] - 48);
+			if ((i + diff) < 0) {
+				b = 0;
+			}
+			else {
+				b = int(B[i + diff] - 48);
+			}
+
+			if ((alen + i) % 2 == 1) {
+				C[i] = L[(a + b) % 13];
+			}
+			else {
+				C[i] = char((b - a) < 0 ? (b - a + 10 + 48) : (b - a + 48));
+			}
 		}
 	}
 	cout << C;
--- test.cpp
+++ test.cpp
@@ -1,11 +1,1 @@
-#include <iostream>  
-#include <iomanip>
-using namespace std;  
-
-
-int main(){  
-    char c;
-    int i[3] = {0};
-    printf("%c",c);
-    system("pause");
-}  
+//
